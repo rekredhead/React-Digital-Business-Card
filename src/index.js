ed6a804c0f0dbc7b6ed74c556abf5fad4ad3bc45
rekredhead <rekredhead@gmail.com>
2022-10-02 09:07:02 +0530

    New commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const root = document.getElementById('root');
+const simpleStaticPage = (
+    <div>
+        <img src="./logo192.png" width="40px" alt="React Logo"></img>
+        <h1>Fun Facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-    firstName: 'Kehan',
-    lastName: 'Rathnayake'
-};
-
-const element = (
-    <h1>
-        Hello, {formatName(user)}
-    </h1>
+        <ol>
+            <li>Why do we need to 'import React from "react"' in our files?</li>
+            <p>To gain access to the React Module - Use react in our program</p>
+            
+            <li>If I were to console.log(page) in index.js, what would show up?</li>
+            <p>An Object would be displayed with the contents of the page JSX</p>
+            
+            <li>What's wrong with this code:${"\
+                    const page = (\
+                    <h1>Hello</h1>\
+                    <p>This is my website</p>\
+                )"
+                }
+            </li>
+            <p>JSX variables can't have more than one element, they need to be inside a div at least</p>
+            
+            <li>What does it mean for something to be 'declarative' instead of "imperative"?</li>
+            <p>(Not sure)</p>
+            
+            <li>What does it mean for something to be "composable"?</li>
+            <p>In general, composable means removable</p>
+        </ol>
+    </div>
 );
 
-ReactDOM.render(element, root);
+const root = document.getElementById('root');
+
+ReactDOM.render(simpleStaticPage, root);
