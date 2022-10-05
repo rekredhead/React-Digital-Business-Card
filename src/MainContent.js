dbef5a05795fe2d7c726c4c6434c7e7c6b1e3c6f
rekredhead <rekredhead@gmail.com>
2022-10-05 21:19:28 +0530

    Commit

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,6 +1,6 @@
 export default function MainContent() {
     return (
-        <>
+        <div className="mainContent">
             <h1>Fun Facts about React</h1>
             <ul>
                 <li>Was first released in 2013</li>
@@ -10,6 +10,6 @@ export default function MainContent() {
                 <li>Powers thousands of enterprise apps, including
                     mobile apps</li>
             </ul>
-        </>
+        </div>
     );
 }
